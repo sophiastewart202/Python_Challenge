9cb49ef4a753143f9f446e61d4e5061e4fca4955
Sophia <sophia.stewart202@protonmail.com>
2021-06-17 16:09:32 -0400

    PyBank: Code to calculate # of months

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,21 +1,26 @@
 #Modules
 import os
 import csv
+from typing import Counter
 
 #Specify the file to read/write to
 csvpath=os.path.join('Resources','pybank_budget_data.csv')
 
 #look in csv path
-with open(csvpath) as text
-    #find the months column count the total number of months (time = number of months)
+with open(csvpath) as datafile:
+    #find the months column and count the total number of months by counting the rows
+    next(datafile) #skip the header
+    csvreader=csv.reader(datafile, delimiter=',') #open in reader mode
+    total_months = sum(1 for row in csvreader)
+    print(total_months)
 
-    #find the net total amount of profits/losses over the entire period (time)
+    #find the net total amount of profits/losses over the entire period (total_months)
 
-    #find the average of the changes in profits/losses over the entire period (time)
+    #find the average of the changes in profits/losses over the entire period (total_months)
 
-    #The greatest increase in profits (date and amount) over the entire period (time)
+    #The greatest increase in profits (date and amount) over the entire period (total_months)
 
-    #The greatest decrease in losses (date and amount) over the entire period (time)
+    #The greatest decrease in losses (date and amount) over the entire period (total_months)
 
     #print analysis to the terminal
 
