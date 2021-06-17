c9838584cbc07ade3e524bbd127537957938626b
Sophia <sophia.stewart202@protonmail.com>
2021-06-17 13:53:32 -0400

    main.py: Initial script setup and pseudo code

---
+++ PyBank/main.py
@@ -0,0 +1,22 @@
+#Modules
+import os
+import csv
+
+#Specify the file to read/write to
+csvpath=os.path.join('Resources','pybank_budget_data.csv')
+
+#look in csv path
+with open(csvpath) as text
+    #find the months column count the total number of months (time = number of months)
+
+    #find the net total amount of profits/losses over the entire period (time)
+
+    #find the average of the changes in profits/losses over the entire period (time)
+
+    #The greatest increase in profits (date and amount) over the entire period (time)
+
+    #The greatest decrease in losses (date and amount) over the entire period (time)
+
+    #print analysis to the terminal
+
+    #export csv with results
