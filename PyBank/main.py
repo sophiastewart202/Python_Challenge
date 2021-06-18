36552ab9b75f4f0fedf4fb626b66af738ade0a4c
Sophia <sophia.stewart202@protonmail.com>
2021-06-18 18:35:30 -0400

    PyBank: Calculation for Average Change and code to print all (thus far) calculated results to terminal.

--- PyBank/main.py
+++ PyBank/main.py
@@ -25,13 +25,15 @@ with open(csvpath) as datafile:
     print(net_total) #does python automatically know which column can be added up? Is that why this works?
 
     #find the average of the changes in profits/losses over the entire period (total_months)
-    # change = []
-    # for row in data:
-    #     next(row)
-    #     change.append(float(row[1])-float(row[0]))
-    # average_change = sum(change)/total_months-1
-    # print(average_change)
-
+    #IDEA 1
+    revenue = []
+    monthly_change = []
+    for row in data:
+         revenue.append(float(row[1]))
+    for i in range(1,len(revenue)):
+        monthly_change.append(revenue[i] - revenue[i-1])
+    avg_change = sum(monthly_change)/(total_months - 1)
+    avg_change = round(avg_change,2)
 
     #The greatest increase in profits (date and amount) over the entire period (total_months)
     # greatest_increase = max(monthly_change)
@@ -40,5 +42,10 @@ with open(csvpath) as datafile:
     #The greatest decrease in losses (date and amount) over the entire period (total_months)
 
     #print analysis to the terminal
+    print("Financial Analysis")
+    print("-----------------------------------")
+    print(f"Total Months: {total_months}")
+    print(f"Total Revenue: $ {net_total}")
+    print(f"Average Change: $ {avg_change}")
 
     #export csv with results
