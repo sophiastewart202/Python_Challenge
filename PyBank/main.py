f2e1eda2b007b338c81ab396035ded30045a2e4e
Sophia <sophia.stewart202@protonmail.com>
2021-06-17 23:16:51 -0400

    PyBank: Calculations for the total months and net total profits/losses

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,22 +3,39 @@ import os
 import csv
 from typing import Counter
 
-#Specify the file to read/write to
+#Specify the file to read to
 csvpath=os.path.join('Resources','pybank_budget_data.csv')
 
 #look in csv path
 with open(csvpath) as datafile:
-    #find the months column and count the total number of months by counting the rows
+
+    #Count the total number of months (by counting the rows)
     next(datafile) #skip the header
     csvreader=csv.reader(datafile, delimiter=',') #open in reader mode
-    total_months = sum(1 for row in csvreader)
+    data = []
+    for row in csvreader: #store each row as one item in the data list  
+        data.append(row)
+    total_months = sum(1 for row in data) #count all the rows
     print(total_months)
 
-    #find the net total amount of profits/losses over the entire period (total_months)
+    #find the net total amount of profits/losses over the entire period
+    net_total=0
+    for row in data:
+        net_total += int(row[1])
+    print(net_total) #does python automatically know which column can be added up? Is that why this works?
 
     #find the average of the changes in profits/losses over the entire period (total_months)
+    # change = []
+    # for row in data:
+    #     next(row)
+    #     change.append(float(row[1])-float(row[0]))
+    # average_change = sum(change)/total_months-1
+    # print(average_change)
+
 
     #The greatest increase in profits (date and amount) over the entire period (total_months)
+    # greatest_increase = max(monthly_change)
+    # print(greatest_increase)
 
     #The greatest decrease in losses (date and amount) over the entire period (total_months)
 
