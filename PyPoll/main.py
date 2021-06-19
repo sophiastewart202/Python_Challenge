56e04a685efd3fe41c1139898c2a17ce0dbe45e8
Sophia <sophia.stewart202@protonmail.com>
2021-06-19 14:59:59 -0400

    PyPoll: Calculations for election results by candidate and code to print to terminal.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,11 +9,13 @@ csvpath=os.path.join('Resources','pypoll_election_data.csv')
 with open(csvpath) as datafile:
     next(datafile) #skip headers
     csvreader = csv.reader(datafile, delimiter=',') #open in reader mode
-    id = []
-    county = []
-    candidates = []
+    data = [] #list of data, row by row
+    id = [] #list of voters by id
+    county = [] #county list
+    candidates = [] #candidates list
 
     for row in csvreader:
+        data.append(row)
 
         id.append(row[0])
 
@@ -25,24 +27,28 @@ with open(csvpath) as datafile:
 
     #The total number of votes cast
     total_votes = int(len(id))
-    print(total_votes)
 
-    #A complete list of candidates who received votes
-    print(candidates)
-
-    #The percentage of votes each candidate won (percentage_votes = count of votes per candidate/total_votes)
-
-
-    #The total number of votes each candidate won (votes_won = count of occurences of each distinct value 
-    # in Candidate column)
+    #The number of votes and percentage of votes each candidate won 
+    def Election_Results():
+        votes_won = 0
+        for candidate in candidates:
+            for row in data:
+                if candidate in row:
+                    votes_won += 1
+        
+            percentage_votes = round((votes_won/total_votes)*100,3)
+            print(f'{candidate}: {votes_won}, {percentage_votes} %')
+            votes_won = 0
 
     #The winner of the election based on popular vote (winner = greatest votes_won)
+    #Create a dictionary
+    #look up winner by max(votes_won)
 
     #print analysis to the terminal 
-    # title = "Election Results"
-    # (print("title") 
-    # print(f'Total Votes: {total_votes}')
-    # for every Candidate in Candidates:
-    #   print(f'| Candidate: {percentage_votes} %, ({votes_won})')
+    print("Election Results")
+    print("--------------------------") 
+    print(f'Total Votes: {total_votes}')
+    print("--------------------------")
+    Election_Results()
 
     #export csv (as text file) to analysis folder with results
